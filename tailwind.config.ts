f6bf5babcbac9d06336de3528ac4f35f02759371
Nicolas Leão <nicolas@dexworks.com>
2023-12-05 00:53:22 -0300

    install flowbite react

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     backgroundImage: {
@@ -14,10 +14,13 @@ const config: Config = {
     extend: {
       colors: {
         "primary-blue": "#106EB0",
-        "primary-orange": "#EC7000",
+        "primary-orange": "#ff9747",
         "primary-green": "#326629",
         "primary-gray": "#33303E",
+        "primary-red": "#d64753",
+        "primary-yellow": "#ffcb47",
         "second-gray": "#4E4B59",
+        "light-gray": "#949494",
         "gray-phone": "#F4F4F4",
         "txt-gray": "#7A77u6",
         "opacity-gray": "rgba(100, 80, 57, 0.1)",
